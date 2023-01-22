b240da341df325d64534200e218aec79650825ac
Ivan-GoIT <educationzac@gmail.com>
2023-01-22 22:52:05 +0200

    fix SearcForm padding

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,21 +1,23 @@
-import css from './SearchForm.module.css'
+import css from './SearchForm.module.css';
 import { useState } from 'react';
+import { Section } from 'components/Section/Section';
 export const SearhForm = () => {
   const [search, setSearch] = useState('');
 
   return (
-    <form className={css.form} >
-      <input
-      
-        name="search"
-        type="text"
-        required
-        value={search}
-        onChange={evt => {
-          setSearch(evt.currentTarget.value);
-        }}
-      />
-      <button type="submit">Search</button>
-    </form>
+    <Section>
+      <form className={css.form}>
+        <input
+          name="search"
+          type="text"
+          required
+          value={search}
+          onChange={evt => {
+            setSearch(evt.currentTarget.value);
+          }}
+        />
+        <button type="submit">Search</button>
+      </form>
+    </Section>
   );
 };
