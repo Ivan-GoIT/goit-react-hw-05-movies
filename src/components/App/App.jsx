80f5b5bb39d06c6ff049a2c222a10dec4cdf47ea
IvanKoshel <educationZak@gmail.com>
2023-01-24 18:59:41 +0200

    delete unuse imports from App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,12 @@
-import { useState, useEffect, useRef } from 'react';
-import { fetchData } from 'helpers';
 import { Header } from 'components/Header/Header';
 import { HomePage } from 'pages/HomePage/HomePage';
-import QueryPath from '../../constants/QueryPath';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { MovieCard } from 'components/MovieCard/MovieCard';
 import { MoviesPage } from 'pages/MoviesPage/MoviesPage';
 import { Cast } from 'components/MovieCard/Cast/Cast';
 import { Reviews } from 'components/MovieCard/Reviews/Reviews';
 import { ToastContainer } from 'react-toastify';
-import { SearhForm } from 'components/SearchForm/SearchForm';
-import { MoviesList } from 'components/MoviesList/MoviesList';
+
 
 import 'react-toastify/dist/ReactToastify.css';
 export const App = () => {
